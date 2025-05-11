bdc07a7cdc070bbd920f14f056503aa46c06acf8
= <=>
2025-05-11 21:08:17 +0600

    Pricing table added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,21 +3,23 @@ import Contact from "@/components/page/Home/Contact";
 import Faqs from "@/components/page/Home/Faqs";
 import LatestPosts from "@/components/page/Home/LatestPosts";
 import Slider from "@/components/page/Home/Slider";
+import { CreativePricing } from "@/components/ui/creative-pricing";
 import { Helmet } from "react-helmet-async";
 
 const Home = () => {
-    return (
-        <>
-            <Helmet>
-                <title>To Found</title>
-            </Helmet>
-            <Slider />
-            <LatestPosts />
-            <Categoties />
-            <Faqs />
-            <Contact />
-        </>
-    );
-}
+  return (
+    <>
+      <Helmet>
+        <title>To Found</title>
+      </Helmet>
+      <Slider />
+      <LatestPosts />
+      <Categoties />
+      <Faqs />
+      <CreativePricing />
+      <Contact />
+    </>
+  );
+};
 
-export default Home
+export default Home;
